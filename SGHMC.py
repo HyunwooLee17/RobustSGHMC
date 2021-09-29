3de0fd8a275a2b6da3cc46804cbb3d3bda01e65f
HyunwooLee17 <lhw941@gmail.com>
2021-09-29 17:19:04 +0900

    amagold bnn

--- SGHMC.py
+++ SGHMC.py
@@ -9,7 +9,6 @@ import random
 import torch.distributions as dist
 import torchbnn as bnn
 import numpy as np
-from copy import deepcopy
 
 datasize=60000
 batch_size=1000
@@ -27,62 +26,24 @@ class BNNmodel(nn.Module):
         x = F.relu(self.fc1(x))
         return F.log_softmax(self.fc2(x),dim=1)
 
-
-
-def step(model,lr,beta,p_buf,mh_train_data):
-    model_old=deepcopy(model)
-    rho=0
-    i=0
-    p_old_neg_buf=[]
+def step(model,lr,weight_decay,alpha):
     for p in model.parameters():
-        p_old_neg_buf.append(-p_buf[i])
-        p.data+=0.5*p_buf[i]
-        AMAGOLD(p,lr,beta,rho,p_buf,i)
-        p.data+=0.5*p_buf[i]
-        i+=1
-    # for data, target in mh_train_data:
-    #     data, target = data.to(device), target.to(device)
-    #     model.zero_grad()
-    #     output = model(data)
-    #     U_new = F.nll_loss(output, target)
-    #     model_old.zero_grad()
-    #     output = model_old(data)
-    #     U_old = F.nll_loss(output, target)
-    #     break
-    
-    # a=(U_old-U_new)*datasize+rho
-    
-    u=torch.log(torch.rand(1))
-    # if u.item()<=a.data.item():
-    #     print('success!')
-    #     return model,p_buf
-    # else:
-    #     return model_old,p_old_neg_buf
-    return model,p_buf
+        if not hasattr(p,'buf'):
+            p.buf = torch.randn(p.size()).to(device)*np.sqrt(lr)
+        # print(p)
+        # i=i+1
+        d_p = p.grad.data
+        d_p.add_(weight_decay, p.data)
+        eps = torch.randn(p.size()).to(device)
+        buf_new = (1-alpha)*p.buf - lr*d_p + (2.0*lr*alpha)**.5*eps
+        p.data.add_(buf_new)
+        p.buf = buf_new
 
-def AMAGOLD(p,lr,beta,rho,p_buf,i):
-    for t in range(T):
-        if t!=0:
-            p.data+=p_buf[i]
-        p_buf_old=deepcopy(p_buf[i])
-        eta=torch.normal(mean=0,std=torch.ones_like(p)*np.sqrt(4*lr*beta)).to(device)
-        U_p=p.grad.data
-        U_p.add_(5e-4,p.data)
-        p_buf[i]=((1-lr*beta)*p_buf_old-lr*U_p+eta)/(1+lr*beta)
-        rho += torch.sum(U_p * (p_buf_old + p_buf[i]))*0.5
-    
-    
 
-    
-
-def train(model,epoch,traindata,mh_train_data):
+def train(model,epoch,traindata):
     model.train()
     # traindata,testdata=getData(batch_size)
-    # loss_fn=torch.nn.NLLLoss(reduction='mea)
-    lr=0.01/datasize
-    p_buf=[]
-    for p in model.parameters():
-        p_buf.append(torch.randn(p.size()).to(device)*np.sqrt(lr))
+    # loss_fn=torch.nn.NLLLoss(reduction='mea
     for batch_idx, (data,target) in enumerate(traindata):
         data, target = data.to(device), target.to(device)
         model.zero_grad()
@@ -96,27 +57,20 @@ def train(model,epoch,traindata,mh_train_data):
         # input()
         # 이러면 loss가 실질적으로 U(theta)의 역할을 한다
         loss.backward()
-        model,p_buf=step(model,lr,0.5,p_buf,mh_train_data)
+        step(model,lr=0.1/datasize,weight_decay=5e-4,alpha=0.01)
         if batch_idx%100==0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(traindata.dataset),
                 100. * batch_idx / len(traindata), loss.data.item()))
 
-def test(model,testdata,epoch):
+def test(model,testdata):
     model.eval()
-    test_loss=0
-    correct=0
     with torch.no_grad():
         for data,target in testdata:
             data, target = data.to(device), target.to(device)
-            output=model(data)
-            test_loss+=F.nll_loss(output,target,reduction='sum')
-            pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
-            correct += pred.eq(target.data.view_as(pred)).cpu().sum()
-    test_loss /= len(testdata.dataset)
-    print('\nIter: {}, Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(epoch,
-        test_loss, correct, len(testdata.dataset),
-        100. * correct / len(testdata.dataset)))
+            pred=model(data)
+            print(F.nll_loss(pred,target))
+
 
 def getData(batch_size):
     mnist_train = dsets.MNIST(root='MNIST_data/',
@@ -137,23 +91,17 @@ def getData(batch_size):
                                         shuffle=True,
                                         drop_last=True)
 
-
-    mh_train_loader = DataLoader(dataset=mnist_train,
-                                        batch_size=datasize, # 배치 크기는 100
-                                        shuffle=True,
-                                        drop_last=True)
-
     test_loader = DataLoader(dataset=mnist_test,
                                         batch_size=10000,
                                         shuffle=True,
                                         drop_last=True)
-    return data_loader,test_loader,mh_train_loader
+    return data_loader,test_loader
 
 if __name__=="__main__":
     M=BNNmodel()
     M.to(device)
-    traindata,testdata,mh_train_data=getData(batch_size)
-    test(M,testdata,-1)
-    for epoch in range(10):
-        train(M, epoch,traindata,mh_train_data)
-        test(M,testdata,epoch)
+    traindata,testdata=getData(batch_size)
+    test(M,testdata)
+    for epoch in range(100):
+        train(M, epoch,traindata)
+        test(M,testdata)
